293870cde994465f9fd4fa1af760136982d9af60
Irina041708 <q-w-i-n-86@rambler.ru>
2022-09-27 11:57:29 +0400

    add Sem2

---
+++ Sem2/Zad1.py
@@ -0,0 +1,11 @@
+# 1.	Напишите программу, которая принимает на вход число N и выдаёт последовательность из N членов.
+# Пример: Для N = 5: 1, -3, 9, -27, 81
+
+
+Number = int(input("Введите число N:"))
+result = []
+for degree in range(Number+1):
+    result.appand(str((-3)**degree))
+
+print(",".join(result), end=",")
+print(".")
